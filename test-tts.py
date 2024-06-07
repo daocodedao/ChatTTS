553d446a5ca7ac4268e650a314dd94d0ff7864b9
linzhiji <linzhiji@ishehui.com>
2024-06-07 16:04:28 +0800

    fix bug

--- test-tts.py
+++ test-tts.py
@@ -10,7 +10,7 @@ from utils.logger_settings import api_logger
 ans_id = getCurTimeStampStr()
 wavDir="./out/"
 outPath = f"{wavDir}{ans_id}.mp3"
-text = "2004年就在 OpenAI 发布可以生成令人瞠目的视频的 Sora \n谷歌披露支持多达 150 万个Token上下文的 Gemini 1.5。\n几天后，Stability AI 最近展示了 Stable Diffusion 3 的预览版。"
+text = "在以色列军队对加沙拉法赫地区的持续军事行动中，存在一种普遍的看法，即即将取得胜利并彻底消灭哈马斯。\n然而，最近的发展表明，以色列可能不会迅速实现其目标，这可能会进一步加剧中东地区的紧张局势。\n尽管投入了大量资金，但拉法赫的进展明显缓慢。\n国际组织报告称，该地区遭受了超过7万吨的集中轰炸，超过了二战期间伦敦、汉堡和德累斯顿等城市 的历史水平。\n尽管遭受如此猛烈的攻击，以色列并未达到消灭哈马斯的目标，哈马斯继续抵抗。"
 
 # text=convert_arabic_to_chinese_in_string(text)
 srcText = cn2an.transform(text, "an2cn")
