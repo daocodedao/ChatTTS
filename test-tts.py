68f4eb19c0b0cd96c2c6036dfbda212e4915fbaa
linzhiji <linzhiji@ishehui.com>
2024-09-02 18:38:59 +0800

    fix bug

--- test-tts.py
+++ test-tts.py
@@ -10,6 +10,9 @@ from utils.logger_settings import api_logger
 ans_id = getCurTimeStampStr()
 wavDir="./out/"
 outPath = f"{wavDir}{ans_id}.mp3"
+
+os.makedirs(wavDir, exist_ok=True)
+
 text = "在以色列军队对加沙拉法赫地区的持续军事行动中，存在一种普遍的看法，即即将取得胜利并彻底消灭哈马斯。\n然而，最近的发展表明，以色列可能不会迅速实现其目标，这可能会进一步加剧中东地区的紧张局势。\n尽管投入了大量资金，但拉法赫的进展明显缓慢。\n国际组织报告称，该地区遭受了超过7万吨的集中轰炸，超过了二战期间伦敦、汉堡和德累斯顿等城市的历史水平。\n尽管遭受如此猛烈的攻击，以色列并未达到消灭哈马斯的目标，哈马斯继续抵抗。"
 
 
